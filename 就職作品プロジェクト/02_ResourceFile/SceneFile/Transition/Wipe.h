0755ab9874692e665924d53bb376127b2a9be0f1
KodoKE2004 <kouta159635@icloud.com>
2025-11-01 10:25:31 +0900

    Implement fade and wipe transitions

--- 就職作品プロジェクト/02_ResourceFile/SceneFile/Transition/Wipe.h
+++ 就職作品プロジェクト/02_ResourceFile/SceneFile/Transition/Wipe.h
@@ -15,15 +15,27 @@ private:
         NUM
     };
 
-   
+    enum class WIPE_PHASE
+    {
+        OUT,
+        IN,
+        FINISH,
+    };
+
+    WIPE_MODE  m_Mode  = WIPE_MODE::LEFT_TO_RIGHT;
+    WIPE_PHASE m_Phase = WIPE_PHASE::OUT;
+    float      m_Rate  = 0.0f;
+
+    void ApplyWipeAmount(float amount);
+
 public:
-    using TransitionBase::TransitionBase; // Camera* を親から
+    using TransitionBase::TransitionBase; // Camera* e
 
     void Initialize() override;
-    void Update(){}           ;
+    void Update()     override;
     void Draw()       override;
     void Finalize()   override;
 
-    void WIPE_IN() ;
+    void WIPE_IN();
     void WIPE_OUT();
 };
