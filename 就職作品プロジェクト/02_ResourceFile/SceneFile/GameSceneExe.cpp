5251ddd2ab73b026eff642015fca0fddf1f636bc
KodoKE2004 <kouta159635@icloud.com>
2026-02-02 17:21:38 +0900

    音源変更

--- 就職作品プロジェクト/02_ResourceFile/SceneFile/GameSceneExe.cpp
+++ 就職作品プロジェクト/02_ResourceFile/SceneFile/GameSceneExe.cpp
@@ -111,25 +111,28 @@ void GameSceneExe::Update(float tick)
     }
 
     // 速くクリアしてた場合の処理
-    if (m_isFastChange)
-    {
-        // 残りの拍が4の倍数になった瞬間終了
-        if (rest % 4 == 0)
-        {
-            rest = 0;
-            m_SegmentFrom = m_FillRatio;
-            PlaySE("explosion", std::nullopt);
-            m_isChange = true;
-            m_BomberElapsed = 0.0f;
-        }
-        const float t = std::clamp(m_BomberElapsed / GetOneBeat(), 0.0f, 1.0f);
-        const float e = Math::Easing::EaseOutQuart(t);
+    //if (m_isFastChange)
+    //{
+    //    // 残りの拍が4の倍数になった瞬間終了
+    //    if (rest % 4 == 0)
+    //    {
+    //        rest = 0;
+    //        m_SegmentFrom = m_FillRatio;
+    //        PlaySE("explosion", std::nullopt);
+    //        m_isChange = true;
+    //        m_BomberElapsed = 0.0f;
+    //    }
+    //    if (m_isChange) 
+    //    {
+    //        const float t = std::clamp(m_BomberElapsed / GetOneBeat(), 0.0f, 1.0f);
+    //        const float e = Math::Easing::EaseOutQuart(t);
 
-        m_FillRatio = m_SegmentFrom + (1.0f - m_SegmentFrom) * e;
-        m_Bomber->SetFillRatio(1.0f - m_FillRatio);
-    }
+    //        m_FillRatio = m_SegmentFrom + (1.0f - m_SegmentFrom) * e;
+    //        m_Bomber->SetFillRatio(1.0f - m_FillRatio);
+    //    }
+    //}
     // ボンバーの更新
-    else if(m_Bomber && GetOneBeat() > 0.0f)
+    if(m_Bomber && GetOneBeat() > 0.0f)
     {
         const float t = std::clamp(m_BomberElapsed / GetOneBeat(), 0.0f, 1.0f);
         const float e = Math::Easing::EaseOutQuart(t);
